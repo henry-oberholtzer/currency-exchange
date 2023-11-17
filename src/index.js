c3d1b1cd562af9e9052c863e2d354ed5981a6c22
Henry Oberholtzer <henryoberholtzer@gmail.com>
2023-11-17 11:19:32 -0800

    troubleshooting printExchange()

--- src/index.js
+++ src/index.js
@@ -1,24 +1,66 @@
 import './css/styles.css';
 import CurrencyExchange from "./js/currency-exchange";
 
+const addToSessionStorage = (name, object) => {
+    sessionStorage.setItem(name, JSON.stringify(object));
+};
+
 const makeExchangeRateCall = () => {
-    return "nothing here yet";
+    if (sessionStorage.exchangeRates) {
+        const ratesStored = JSON.parse(sessionStorage.countryCodes);
+        printExchange(ratesStored);
+    }   else {
+        CurrencyExchange.getExchangeRates()
+            .then((response) => {
+                if (response["conversion_rates"]) {
+                    printExchange(response);
+                    addToSessionStorage("exchangeRates", response);
+                }   else {
+                    printExchangeError(response);
+                }
+            });
+    }
+};
+const printExchange = (response) => {
+
+    const ratesObject = response["conversion_rates"];
+    const currencyFrom = document.getElementById("currencyFrom").value;
+    const currencyTo = document.getElementById("currencyTo").value;
+    const ratioFrom = ratesObject[currencyFrom];
+    const ratioTo = ratesObject[currencyTo];
+    const currencyFromAmount = parseInt(document.getElementById("currencyFromAmount").value);
+    const currencyToAmount = document.getElementById("currencyToAmount");
+    const conversion = (ratioFrom * currencyFromAmount) * ratioTo;
+    console.log(conversion);
+    currencyToAmount.append(conversion);
+};
+
+const printExchangeError = (response) => {
+    return response;
 };
 
 const makeCountryCodesCall = () => {
-    CurrencyExchange.getCountryCodes()
-        .then((response) => {
-            if (response["supported_codes"]) {
-                printCurrencyList(response);
-            } else {
-                printCurrencyError(response);
-            }
-        });
+    if (sessionStorage.countryCodes) {
+        const codesStored = JSON.parse(sessionStorage.countryCodes);
+        printCurrencyList(codesStored);
+    } else {
+        CurrencyExchange.getCountryCodes()
+            .then((response) => {
+                if (response["supported_codes"]) {
+                    printCurrencyList(response);
+                    addToSessionStorage("countryCodes", response);
+                } else {
+                    printCurrencyError(response);
+                }
+            });
+    }
 };
 
 const printCurrencyList = (response) => {
     const currencyToOptions = document.getElementById("currencyTo");
     const currencyFromOptions = document.getElementById("currencyFrom");
+    currencyToOptions.innerHTML = "";
+    currencyFromOptions.innerHTML = "";
     response["supported_codes"].forEach((currencyName) => {
         const optionTo = document.createElement("option");
         optionTo.setAttribute("value", currencyName[0]);
@@ -29,14 +71,28 @@ const printCurrencyList = (response) => {
         if (currencyName[0] === "USD") {
             optionFrom.setAttribute("selected", true);
         }
+        if (currencyName[0] === "PLN") {
+            optionTo.setAttribute("selected", true);
+        }
         currencyFromOptions.append(optionFrom);
         currencyToOptions.append(optionTo);
     });
 };
 
 const printCurrencyError = (response) => {
-    return response;
+    const currencyToOptions = document.getElementById("currencyTo");
+    const currencyFromOptions = document.getElementById("currencyFrom");
+    const optionToFailed = document.createElement("option");
+    const optionFromFailed = document.createElement("option");
+    optionToFailed.setAttribute("disabled", true);
+    optionFromFailed.setAttribute("disabled", true);
+    optionToFailed.append(response);
+    optionFromFailed.append(response);
+    currencyToOptions.append(optionToFailed);
+    currencyFromOptions.append(optionFromFailed);
 };
 
-document.getElementById("form").addEventListener("submit", makeExchangeRateCall);
-window.addEventListener("load", makeCountryCodesCall);
+window.addEventListener("load", () => {
+    makeCountryCodesCall();
+    makeExchangeRateCall();
+});
